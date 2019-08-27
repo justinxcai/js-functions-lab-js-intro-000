76f138d645468ea33ffbd66b069c52b3f3d7b412
Justin Cai <justinxcai@gmail.com>
2019-08-27 19:39:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function happyHolidays () {return "Happy holidays!"}
-
-function happyHolidaysTo(name){return `Happy holidays, ${name}!`
-}
-function happyHolidayTo(holiday, name){
-  return `Happy ${holiday}, ${name}!`
-}
-function holidayCountdown(holiday, days){
-  return `It\'s ${days} days until ${holiday}!`
-}
+function hapyholidays() {return `Happy holidays!`}
+function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
+function happyHolidayTo(holiday, name) {return `happy ${holiday}, ${name}!`}
+function holidayCountdown(days, holiday) {return `It\'s ${days} days until ${holiday}!`}
